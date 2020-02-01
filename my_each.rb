b5bf059bbf8d6b7e71c41f4da0bf8c0694ed6bb5
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-02-01 01:47:02 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,8 @@
 def my_each array 
-  if array != []
-    i = 0 
-    while i < array.size 
-      yield array[i]
+  i = 0 
+  while i < array.size 
+    yield array[i]
       i += 1 
-    end 
-    array 
-  end
+  end 
+  array 
 end
